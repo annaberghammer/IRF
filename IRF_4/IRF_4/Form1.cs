fa0538d472354391129c1ac525b2b8e6b8ba71bf
Anna Berghammer <anna.berghammer@stud.uni-corvinus.hu>
2022-10-06 22:34:45 +0100

    Load data

--- IRF_4/IRF_4/Form1.cs
+++ IRF_4/IRF_4/Form1.cs
@@ -12,9 +12,20 @@ namespace IRF_4
 {
     public partial class Form1 : Form
     {
+        List<Flat> flats = new List<Flat>();
+        RealEstateEntities context = new RealEstateEntities();
+
+        
         public Form1()
         {
             InitializeComponent();
+
+            LoadData();
+        }
+
+        void LoadData()
+        {
+            flats = context.Flats.ToList();
         }
     }
 }
