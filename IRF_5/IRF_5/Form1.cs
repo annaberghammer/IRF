1816b826f99b37ba4b83fe94f12edb88d4d2b225
Anna Berghammer <anna.berghammer@stud.uni-corvinus.hu>
2022-10-21 13:23:51 +0100

    Call the web service

--- IRF_5/IRF_5/Form1.cs
+++ IRF_5/IRF_5/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IRF_5.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,20 @@ namespace IRF_5
         public Form1()
         {
             InitializeComponent();
+        }
+
+        void GetExchangeRate()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
