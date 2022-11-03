54c759a8da174f6d84b2161d3764f920113301a1
Anna Berghammer <anna.berghammer@stud.uni-corvinus.hu>
2022-11-03 09:36:07 +0000

    Labdagyár létrehozása

---
+++ IRF_06/IRF_06/Entities/BallFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_06.Entities
+{
+    internal class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+
+    }
+}
