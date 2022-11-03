a42371907511519cea073d850a44e7e6d3407d92
Anna Berghammer <anna.berghammer@stud.uni-corvinus.hu>
2022-11-03 10:40:35 +0000

    Absztrakciók felhasználása a gyárakban

--- IRF_06/IRF_06/Entities/CarFactory.cs
+++ IRF_06/IRF_06/Entities/CarFactory.cs
@@ -4,14 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace IRF_06.Entities
 {
-    public class BallFactory : IToyFactory
+    public class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
