00fce8943cc235f6a95ceaa3527b02787bf0fd38
Anna Berghammer <anna.berghammer@stud.uni-corvinus.hu>
2022-11-03 10:25:08 +0000

    Absztrakció felhasználása a labdában

--- IRF_06/IRF_06/Entities/Ball.cs
+++ IRF_06/IRF_06/Entities/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IRF_06.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -8,30 +9,11 @@ using System.Windows.Forms;
 
 namespace IRF_06.Entities
 {
-    public class Ball : Label
+    public class Ball : Toy
     {
-        public Ball()
-        {
-            AutoSize = false;
-            Height = 50;
-            Width = 50;
-
-            Paint += Ball_Paint;
-        }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-
-        protected void DrawImage(Graphics g)
+        protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
-        }
-
-        public void MoveBall()
-        {
-            Left += 1;
         }
     }
 }
