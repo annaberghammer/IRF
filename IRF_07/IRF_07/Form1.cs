82d403b3c7c7052edd94bbfda0e941bf859cce3a
Anna Berghammer <anna.berghammer@stud.uni-corvinus.hu>
2022-11-10 14:16:04 +0000

    Adatok betöltése

--- IRF_07/IRF_07/Form1.cs
+++ IRF_07/IRF_07/Form1.cs
@@ -12,9 +12,16 @@ namespace IRF_07
 {
     public partial class Form1 : Form
     {
+        List<Tick> Ticks;
+        PortfolioEntities context = new PortfolioEntities();
+
         public Form1()
         {
             InitializeComponent();
+
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+
         }
     }
 }
